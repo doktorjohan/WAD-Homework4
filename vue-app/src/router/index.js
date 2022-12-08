6898729c64442be7f2ffc12c5b170c974f29d0e4
doktorjohan <johanhollak1@gmail.com>
2022-12-08 13:23:21 +0200

    added LoginView component, added auth check to router

--- vue-app/src/router/index.js
+++ vue-app/src/router/index.js
@@ -1,12 +1,22 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SignUpView from '../views/SignUpView.vue';
+import LoginView from "@/views/LoginView";
+import auth from "@/auth";
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    beforeEnter: async(to, from, next) => {
+      let authResult = await auth.authenticated()
+      if (!authResult) {
+        next('/login')
+      } else {
+        next()
+      }
+    }
   },
   {
     path: '/signup',
@@ -16,13 +26,17 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     //component: () => import(/* webpackChunkName: "about" */ '../views/AddPostView.vue')
     component: SignUpView
-
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
   }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
 })
 
 export default router
