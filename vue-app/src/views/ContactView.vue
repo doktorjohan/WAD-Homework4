8f446cc771a0bb5e1efb0e67a81045d8a187133c
Artur Kreegipuu <artur@kreegipuu.net>
2022-12-11 21:15:24 +0200

    contactview footer fixed

--- vue-app/src/views/ContactView.vue
+++ vue-app/src/views/ContactView.vue
@@ -76,14 +76,17 @@ p {
   font-family: "Comic Sans MS",serif;
 }
 footer {
-  position: relative;
   background-color: #69a2b0ff;
   border-radius: 5px;
   padding: 10px;
   text-align: center;
   box-shadow: 0 0 14px 0 rgba(0, 0, 0, 0.75);
   margin-top: auto;
-  margin-bottom: 0px;
+  position: fixed;
+  bottom: 0px;
+  left: 0px;
+  right: 0px;
+
 
 }
 img {
